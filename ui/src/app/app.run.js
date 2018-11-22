51d90baf4dae2008de441fd1fc425aad3e4b9081
mariano <mariano.dominguez@adaptio.com.ar>
2018-11-22 11:35:25 -0300

    change color

--- ui/src/app/app.run.js
+++ ui/src/app/app.run.js
@@ -147,9 +147,9 @@ export default function AppRun($rootScope, $window, $injector, $location, $log, 
             }
             if (angular.isDefined(to.data.pageTitle)) {
                 $translate(to.data.pageTitle).then(function (translation) {
-                    $rootScope.pageTitle = 'ThingsBoard | ' + translation;
+                    $rootScope.pageTitle = 'Sense It | ' + translation;
                 }, function (translationId) {
-                    $rootScope.pageTitle = 'ThingsBoard | ' + translationId;
+                    $rootScope.pageTitle = 'Sense It | ' + translationId;
                 });
             }
         })
--- ui/src/app/layout/home.controller.js
+++ ui/src/app/layout/home.controller.js
@@ -17,7 +17,7 @@ import $ from 'jquery';
 
 /* eslint-disable import/no-unresolved, import/default */
 
-import logoSvg from '../../svg/logo_title_white.svg';
+import logoSvg from '../../svg/home.svg';
 
 /* eslint-enable import/no-unresolved, import/default */
 
